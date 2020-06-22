35b0cf3af7664d02b223cf6381ca51dd8e772ea8
Seiryu KAKUTA <seiryu.kakuta@konicaminolta.com>
2020-06-22 14:52:06 +0900

    FizzBuzzTest.ino

--- FizzBuzzTest/FizzBuzzTest.ino
+++ FizzBuzzTest/FizzBuzzTest.ino
@@ -1,9 +1,6 @@
 #define WAIT_TIME           (500) // [ms]
 
-
-int g_display_mode;
-int g_count = 0;
-
+int i = 0;
 void setup() 
 {
 	Serial.begin(115200);
@@ -11,23 +8,25 @@ void setup()
 
 void loop() 
  {
-  int i = 0;
-  for(i=0;i<100;i++){
-    	if(i% 15 == 0 ) {
-        	Serial.print("FizzBuzz\n"); // output string (and line feed)
+  if(i<100)
+  {
+    Serial.print(i,DEC); // output string (and line feed)
+    	if(i% 15 == 0) {
+        	Serial.print(":FizzBuzz\n"); // output string (and line feed)
         }
         else if(i % 5 == 0) {
-        	Serial.print("Fizz\n"); // output string (and line feed)
+        	Serial.print(":Fizz\n"); // output string (and line feed)
         }
         else if(i % 3 ==0) {
-        	Serial.print("Buzz\n"); // output string (and line feed)
+        	Serial.print(":Buzz\n"); // output string (and line feed)
         }
         else{
-          Serial.print(i,DEC); // output string (and line feed)
           Serial.print("\n"); // output string (and line feed)
         }
           delay(WAIT_TIME);
+          i++;
   }
-  }
+}
+ 
         
    
